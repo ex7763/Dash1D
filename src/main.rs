6b9027479bb8489b959ca1949eeca6290d94e0b6
Hsu, Po-Chun <eddie55020@gmail.com>
2024-01-12 00:51:10 +0800

    Random bomb. Gradually increase difficulty.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::iter::once;
+use std::time::SystemTime;
 use is_close::is_close;
-use is_close::default;
-use macroquad::ui::{hash, root_ui, widgets};
 use macroquad::prelude::*;
+use macroquad::rand::{srand, gen_range};
+// use rand::Rng;
 
 
 fn window_conf() -> Conf {
@@ -25,21 +25,26 @@ enum Status {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let mut x = 256.0;
+    // let mut rng = rand::thread_rng();
+    let d = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .expect("Duration since UNIX_EPOCH failed");
+    srand(d.as_secs());
+
+    let mut x: f32 = gen_range(200.0, 512.0 - 200.0);
     let mut y = 128.0 - 12.0;
 
     let mut score: u32 = 0;
-    let speed = 1.0;
+    let mut speed = 3.0;
     let mut xv = speed;
-    let mut xa = 0.0;
-    let eps = 1e-9;
     let eps = 1e-5;
 
     let mut status = Status::Idle;
 
 
-    let mut boob_x = 400.0;
-    let mut boob_r = 5.0;
+    let mut bomb_x =  gen_range(400.0, 500.0);
+    let mut bomb_r = 5.0;
+    let mut bomb_speed = 1.5;
 
     let mut last_time = get_time();
     let desired_frame_time: f64 = 1.0 / 60.0;  // 60 fps
@@ -64,16 +69,15 @@ async fn main() {
         );
 
         draw_rectangle(x - 12.0, y, 24.0, 24.0, GREEN);
-        draw_circle_lines(boob_x, 128.0, boob_r, 3.0, RED);
-        boob_r += 0.3;
+        draw_circle_lines(bomb_x, 128.0, bomb_r, 3.0, RED);
 
         if status == Status::Die {
-            draw_text("You Die", 110.0, 100.0, 96.0, RED);
+            draw_text("You Die", 110.0, 152.0, 92.0, RED);
         }
 
         if status == Status::Idle {
-            if (x < boob_x + boob_r + 3.0 && x > boob_x + boob_r - 3.0) ||
-               (x < boob_x - boob_r + 3.0 && x > boob_x - boob_r - 3.0) {
+            if (x < bomb_x + bomb_r + 3.0 && x > bomb_x + bomb_r - 3.0) ||
+               (x < bomb_x - bomb_r + 3.0 && x > bomb_x - bomb_r - 3.0) {
                 status = Status::Die;
                 println!("Die");
             }
@@ -83,10 +87,10 @@ async fn main() {
                         match key {
                             KeyCode::Space=> {
                                 if xv == speed {
-                                    xv += 5.0;
+                                    xv += 15.0;
                                 }
                                 if xv == -speed {
-                                    xv -= 5.0;
+                                    xv -= 15.0;
                                 }
                             },
                             _ => {}
@@ -109,42 +113,59 @@ async fn main() {
             }
             else {
                 if xv > speed {
-                    xv = f32::max(speed, xv - 0.3);
+                    xv = f32::max(speed, xv - 3.0);
                 }
                 else if xv < -speed {
-                    xv = f32::min(-speed, xv + 0.3);
+                    xv = f32::min(-speed, xv + 3.0);
                 }
                 status = Status::Dash;
             }
-        }
+            score += 1;
 
-        x += xv;
-        x = f32::max(f32::min(x, 512.0), 0.0);
+            x += xv;
+            x = f32::max(f32::min(x, 512.0), 0.0);
 
-        if x == 512.0 {
-            xv = -speed;
-        }
-        if x == 0.0 {
-            xv = speed;
+            if x == 512.0 {
+                xv = -speed;
+            }
+            if x == 0.0 {
+                xv = speed;
+            }
         }
         // println!("{x}, {xv}");
 
-        score += 1;
+
+        // Update Bomb
+        if bomb_r >= 100.0 {
+            bomb_x =  gen_range(2.0, 512.0 - 20.0);
+            while f32::abs(bomb_x - x) < 100.0 {
+                bomb_x =  gen_range(20.0, 512.0 - 20.0);
+            }
+            bomb_r = 5.0;
+            bomb_speed += 0.1;
+            speed += 0.3;
+            if xv > 0.0 {
+                xv = speed;
+            }
+            else {
+                xv = -speed;
+            }
+        }
+        else {
+            bomb_r += bomb_speed;
+        }
+
 
         let time_passed = get_time() - last_time;
-        // println!("{time_passed}");
-        // if time_passed > desired_frame_time {
-        //     println!("{time_passed}");
-        //     last_time = get_time();
-        //
-        //     let time_to_sleep = (minimum_frame_time - frame_time) * 1000.;
-        //     println!("Sleep for {}ms", time_to_sleep);
-        //     std::thread::sleep(std::time::Duration::from_millis(time_to_sleep as u64));
-        //
-        //     next_frame().await;
-        // }
+        if time_passed < desired_frame_time {
+            // println!("{time_passed}");
 
+            let time_to_sleep = (desired_frame_time - time_passed) * 1000.;
+            // println!("Sleep for {}ms", time_to_sleep);
+            std::thread::sleep(std::time::Duration::from_millis(time_to_sleep as u64));
+
+            next_frame().await;
+        }
         last_time = get_time();
-        next_frame().await;
     }
 }
